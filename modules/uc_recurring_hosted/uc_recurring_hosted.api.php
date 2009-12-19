2d38e8a4c2e68c4d3c3ef190228f26102b26a2dc
Chris Hood <chris@univate.com.au>
2009-12-19 04:16:49 +0000

    #569300 by Amitaibu: Create Paypal WPP recurring fee handler

---
+++ modules/uc_recurring_hosted/uc_recurring_hosted.api.php
@@ -0,0 +1,14 @@
+<?php
+// $Id$
+
+function hook_uc_recurring_hosted_subscription_alter(&$object) {
+
+}
+
+function hook_uc_recurring_hosted_subscription_save($object) {
+
+}
+
+function hook_uc_recurring_hosted_subscription_delete($id, $type = 'rfid') {
+
+}
